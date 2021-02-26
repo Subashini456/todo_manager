375a5a14fa9fa79b96774dc7acd18bea237c02f1
Subashini <subashiniravi492@gmail.com>
2021-02-26 17:26:50 +0530

    can do single todo /todos/id

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Todo - resourse
+  # - to view list of resource - index
+  # - to view single resource - show
+  # - to create a new resource
+  # - resource :todos --> rails get all the routes by default
+
   get "todos", to: "todos#index"
+  get "todos/:id", to: "todos#show"
 end
